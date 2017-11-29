c8286a1cb99f8ddbb41a9d03c145551417e82a04
David Matson <githubdavid@gmail.com>
2017-11-29 01:13:43 -0800

    Test Mobirise website built website

--- index_files/index.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20171122
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('index_files/indexMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
