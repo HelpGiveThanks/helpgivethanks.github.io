f2cd2257f743794ea96379514c597e8d22f4147f
David Matson <githubdavid@gmail.com>
2019-01-07 12:14:00 -0800

    Test of icon for references 2

--- assets/mbr-switch-arrow/mbr-switch-arrow (TEST).js
+++ assets/mbr-switch-arrow/mbr-switch-arrow (TEST).js
@@ -19,20 +19,20 @@ if (!isBuilder) {
             if (!$iscollapsing.hasClass('collapsing')) {
                 if ($id.indexOf('toggle') != -1){
                     if ($(this).hasClass('collapsed')) {
-                        $(this).find('span.sign').removeClass('mbri-arrow-down').addClass('mbri-arrow-up'); 
+                        $(this).find('span.sign').removeClass('mbr-iconfont fa-arrow-circle-right fa').addClass('mbr-iconfont fa-arrow-circle-down fa');
                     }
                     else{
-                        $(this).find('span.sign').removeClass('mbri-arrow-up').addClass('mbri-arrow-down'); 
+                        $(this).find('span.sign').removeClass('mbr-iconfont fa-arrow-circle-down fa').addClass('mbr-iconfont fa-arrow-circle-right fa'); 
                     }
                 }
                 else if ($id.indexOf('accordion')!=-1) {
                     var $accordion =  $(this).closest('.accordionStyles ');
-                
+
                     $accordion.children('.card').each(function() {
-                        $(this).find('span.sign').removeClass('mbri-arrow-up').addClass('mbri-arrow-down'); 
+                        $(this).find('span.sign').removeClass('mbr-iconfont fa-arrow-circle-down fa').addClass('mbr-iconfont fa-arrow-circle-right fa');
                     });
                     if ($(this).hasClass('collapsed')) {
-                        $(this).find('span.sign').removeClass('mbri-arrow-down').addClass('mbri-arrow-up'); 
+                        $(this).find('span.sign').removeClass('mbr-iconfont fa-arrow-circle-right fa').addClass('mbr-iconfont fa-arrow-circle-down fa');
                     }
                 }
             }
